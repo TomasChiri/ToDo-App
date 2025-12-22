62758e56d84e53f725ed4618d4080af9de7cc3e5
Tomas Chirino <tomasch2000@hotmail.com>
2025-12-22 13:00:53 -0300

    Added date

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 import functions
 import time
 
+now = time.strftime("%b %d, %Y %H:%M:%S")
+
+print("It is", now)
+
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
